122834fa32735249a703b4906899dbe37b3da9a2
hubert <e.hubert.luong@wesang.com>
2023-06-23 16:49:31 +0700

    save template

--- app/views.py
+++ app/views.py
@@ -1,5 +1,4 @@
 from flask import render_template, Blueprint
-from config import BaseConfig
 
 main_blueprint = Blueprint('main', __name__)
 
@@ -7,6 +6,3 @@ main_blueprint = Blueprint('main', __name__)
 @main_blueprint.route('/')
 def index():
     return render_template('index.html')
-
-def static_media(file_name):
-    return BaseConfig.static_media(path=file_name)
--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,6 @@ import click
 from app import create_app, db, models, forms
 
 app = create_app()
-app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 
 # flask cli context setup
